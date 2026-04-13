24e960e4d281ff0b1f391e7fa80a76f06d3e5f7f
apw <apiwatz.lee@gmail.com>
2026-04-13 17:04:36 +0700

    feat:controller function

--- src/app/shape/page.tsx
+++ src/app/shape/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
+
 import './shape.css';
 import { Card } from 'antd';
 import { Divider } from 'antd';
@@ -16,19 +17,47 @@ const itemsShape: string[] = [
   'circle',
 ];
 
+const style = {
+  card: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 400,
+    height: 230,
+    padding: 90,
+    cursor: 'pointer',
+    border: 'none',
+  },
+};
+
 const Layout: React.FC = () => {
   const { t } = useTranslation();
-  const style = {
-    card: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: 400,
-      height: 230,
-      padding: 90,
-      cursor: 'pointer',
-      border: 'none',
-    },
+  const [shapes, setShapes] = useState<string[]>(itemsShape);
+
+  const handlePrev = () => {
+    const firstItems = shapes[0];
+    const othersItems = shapes.slice(1);
+    const prev = [...othersItems, firstItems];
+    setShapes(prev);
+  };
+
+  const handleForward = () => {
+    const lastItems = shapes[shapes.length - 1];
+    const othersItems = shapes.slice(0, shapes.length - 1);
+    const forward = [lastItems, ...othersItems];
+    setShapes(forward);
+  };
+
+  const handleSwap = () => {
+    const firstGroup = shapes.slice(0, shapes.length / 2);
+    const secondGroup = shapes.slice(shapes.length / 2);
+    const swap = [...secondGroup, ...firstGroup];
+    setShapes(swap);
+  };
+
+  const handleShuffle = () => {
+    const shuffle = [...shapes].sort(() => Math.random() - 0.5);
+    setShapes(shuffle);
   };
 
   return (
@@ -37,18 +66,18 @@ const Layout: React.FC = () => {
 
       {/* controller */}
       <div className='controller-container'>
-        <div className='move-shape'>
+        <div className='move-shape' onClick={handlePrev}>
           <Controller arrow='prev' />
           <div className='tag'>{t('layout.forward_prev')}</div>
         </div>
 
-        <div className='move-position'>
+        <div className='move-position' onClick={handleSwap}>
           <Controller arrow='up' className='card-up' />
           <Controller arrow='down' className='card-down' />
           <div className='tag'>{t('layout.up_down')}</div>
         </div>
 
-        <div className='move-shape'>
+        <div className='move-shape' onClick={handleForward}>
           <Controller arrow='forward' />
           <div className='tag'>{t('layout.forward_prev')}</div>
         </div>
@@ -58,8 +87,8 @@ const Layout: React.FC = () => {
 
       {/* shape */}
       <div className='shape-container'>
-        {itemsShape.map((item) => (
-          <Card hoverable style={style.card} key={item}>
+        {shapes.map((item) => (
+          <Card hoverable style={style.card} key={item} onClick={handleShuffle}>
             <div className={item}></div>
           </Card>
         ))}
