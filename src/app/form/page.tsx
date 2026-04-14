d0641301fd88a5589739e69bd4a33c637d4f308f
apw <apiwatz.lee@gmail.com>
2026-04-14 17:58:03 +0700

    fix:hydration mismatch error

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './form.css';
 import { useTranslation } from 'react-i18next';
 import ApplicantForm from '../components/ApplicantForm';
@@ -8,6 +8,15 @@ import ApplicantTable from '../components/ApplicantTable';
 
 const FormPage: React.FC = () => {
   const { t } = useTranslation();
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <div className='container'>
--- src/app/hooks/localStorage.tsx
+++ src/app/hooks/localStorage.tsx
@@ -1,4 +1,5 @@
 export const getDataFromLocalStorage = (key: string, defaultValue: T): T => {
+  if (typeof window === 'undefined') return defaultValue;
   const data = localStorage.getItem(key);
   if (data) {
     return JSON.parse(data) as T;
@@ -7,5 +8,6 @@ export const getDataFromLocalStorage = (key: string, defaultValue: T): T => {
 };
 
 export const setDataToLocalStorage = (key: string, value: any): void => {
+  if (typeof window === 'undefined') return;
   localStorage.setItem(key, JSON.stringify(value));
 };
