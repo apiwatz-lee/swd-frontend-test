4dafdec0a2abdb691f770a1e7108a607e704497b
apw <apiwatz.lee@gmail.com>
2026-04-15 23:51:57 +0700

    fix:set default language to English

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -3,11 +3,9 @@ import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import th from './locales/th.json';
 
-const savedLanguage = localStorage.getItem('user-language') || 'en';
-
 i18n.use(initReactI18next).init({
   fallbackLng: 'en',
-  lng: savedLanguage,
+  lng: 'en',
   resources: {
     en: {
       translation: en,
